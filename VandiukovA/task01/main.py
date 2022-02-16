9819fbd0c04fbacf7bb0bd72ff1d80fe99e27fd2
VandiukovA <vandyuckov0@yandex.ru>
2022-02-16 18:00:07 +0300

    fixed

--- VandiukovA/task01/main.py
+++ VandiukovA/task01/main.py
@@ -1,5 +1,7 @@
 import sys
-def main(args:list):
+
+
+def main(args: list):
     result = ""
     string_to_encrypt = "a b c d e f g h i j k l m n o p q r s t u v w x y z " \
                         "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z " \
@@ -8,7 +10,8 @@ def main(args:list):
     if args[1].lower() == 'e':
         for alphabet_index in args[2]:
             try:
-                current_index = string_to_encrypt_splitted.index(alphabet_index)
+                current_index = string_to_encrypt_splitted.index(
+                    alphabet_index)
                 if alphabet_index.isupper():
                     if int(current_index) + int(args[3]) > 52:
                         modified_index = int(current_index) + int(args[3]) - 26
@@ -24,7 +27,7 @@ def main(args:list):
                         modified_index = current_index + int(args[3])
                         result += string_to_encrypt_splitted[modified_index]
                 elif alphabet_index.isdigit():
-                    if int(current_index) + int(args[3]) > 62:
+                    if int(current_index) + int(args[3]) > 61:
                         modified_index = int(current_index) + int(args[3]) - 10
                         result += string_to_encrypt_splitted[modified_index]
                     else:
@@ -37,7 +40,8 @@ def main(args:list):
     elif args[1].lower() == 'd':
         for alphabet_index in args[2]:
             try:
-                current_index = string_to_encrypt_splitted.index(alphabet_index)
+                current_index = string_to_encrypt_splitted.index(
+                    alphabet_index)
                 if alphabet_index.isupper():
                     if int(current_index) - int(args[3]) < 26:
                         modified_index = int(current_index) - int(args[3]) + 26
@@ -64,6 +68,8 @@ def main(args:list):
         print(result)
     else:
         print("Invalid decrypt/encrypt parameter - press e or d")
+    return result
+
 
 if __name__ == '__main__':
     main(sys.argv)
