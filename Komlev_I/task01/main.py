283b3c1dbdd6fea220a6056933c662fd4485ab53
Ilya Komlev <iekoml@mail.ru>
2022-02-20 18:14:52 +0300

    Review 2.0
    Fixed ceaser function
    Fixed encryption function

--- Komlev_I/task01/main.py
+++ Komlev_I/task01/main.py
@@ -4,17 +4,17 @@ LOWER = string.ascii_lowercase
 UPPER = string.ascii_uppercase
 DIGIT = string.digits
 PUNCT = string.punctuation
-ALL_TEXT = string.ascii_letters+DIGIT+PUNCT
+ALL_TEXT = string.ascii_letters + DIGIT + PUNCT
 
 
-def encryption(category: str, char: str, key: int):
-    mod_char = category[(category.find(char) + key) % len(category)]
-    return mod_char
+def encryption(alphabet: str, char: str, key: int):
+    new_char = alphabet[(alphabet.index(char) + key) % len(alphabet)]
+    return new_char
 
 
 def check_text(text: str):
     for char in text:
-        if char not in ALL_TEXT:
+        if char not in ALL_TEXT+" ":
             print(f"Invalid symbol = {char} in input text")
             sys.exit(-1)
 
@@ -38,15 +38,21 @@ def check_argument(arg: int):
         sys.exit(-1)
 
 
-def ceaser(text: list, key: int):
-    for i in range(len(text)):
-        if text[i].islower():
-            text[i] = encryption(LOWER, text[i], key)
-        elif text[i].isupper():
-            text[i] = encryption(UPPER, text[i], key)
-        elif text[i].isdigit():
-            text[i] = encryption(DIGIT, text[i], key)
-    return text
+def ceaser(text: str, key: int) -> str:
+    result = []
+    for char in text:
+        if char.islower():
+            new_char = encryption(LOWER, char, key)
+            result.append(new_char)
+        elif char.isupper():
+            new_char = encryption(UPPER, char, key)
+            result.append(new_char)
+        elif char.isdigit():
+            new_char = encryption(DIGIT, char, key)
+            result.append(new_char)
+        else:
+            result.append(char)
+    return "".join(result)
 
 
 def main(args: list):
@@ -55,12 +61,13 @@ def main(args: list):
     check_text(str(args[2]))
     check_key(str(args[3]))
     if args[1] == 'e':
-        print("".join(ceaser(list(args[2]), int(args[3]))))
+        print(ceaser(str(args[2]), int(args[3])))
     elif args[1] == 'd':
-        print("".join(ceaser(list(args[2]), -int(args[3]))))
+        print(ceaser(str(args[2]), -int(args[3])))
 
 
 if __name__ == '__main__':
-    main(["main.py", "e", "aBcd*123", "1"])
+    main(["main.py", "e", "aBcd *123", "1"])
     main(["main.py", "d", "aBcd,123", "1"])
     main(["main.py", "e", "aBcd", "1"])
+    main(["main.py", "e", "aBcd", "1", "adsf"])
