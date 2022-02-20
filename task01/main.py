9de243d527a9050de1f0e545ae5be500ae74713b
Sveta <ms.glushakova.2000@mail.ru>
2022-02-21 01:12:51 +0300

    Внесены изменения в файл main.py

--- task01/main.py
+++ task01/main.py
@@ -1,31 +1,56 @@
 import sys
 
 
+def Enc_or_dec(cod: str, str_original: list, k: int):
+    str_code = str_original
+    int_i = 0
+    print(str_original)
+    if cod == 'e':
+        for symbol in str_original:
+            ascii_symbol = ord(symbol)
+            if '1' <= symbol <= '9':
+                if (ascii_symbol + k) > 57:
+                    str_code[int_i] = chr(ascii_symbol - int((ascii_symbol + k) % 10))
+                else:
+                    str_code[int_i] = chr(ascii_symbol + k)
+            elif 65 <= ascii_symbol <= 90:
+                if (ascii_symbol + k) > 90:
+                    str_code[int_i] = chr(ascii_symbol - int((ascii_symbol + k) % 26))
+                else:
+                    str_code[int_i] = chr(ascii_symbol + k)
+            else:
+                if (ascii_symbol + k) > 122:
+                    str_code[int_i] = chr(ascii_symbol - int((ascii_symbol + k) % 26))
+                else:
+                    str_code[int_i] = chr(ascii_symbol + k)
+            int_i += 1
+    if cod == 'd':
+        for symbol in str_original:
+            ascii_symbol = ord(symbol)
+            if '1' <= symbol <= '9':
+                if (ascii_symbol - k) < 48:
+                    str_code[int_i] = chr(ascii_symbol - k + 10)
+                else:
+                    str_code[int_i] = chr(ascii_symbol - k)
+            elif 65 <= ascii_symbol <= 90:
+                if (ascii_symbol - k) < 65:
+                    str_code[int_i] = chr(ascii_symbol - k + 26)
+                else:
+                    str_code[int_i] = chr(ascii_symbol - k)
+            else:
+                if (ascii_symbol - k) < 97:
+                    str_code[int_i] = chr(ascii_symbol - k + 26)
+                else:
+                    str_code[int_i] = chr(ascii_symbol - k)
+            int_i += 1
+    print(str_code)
+
 def main(args):
-    shifr = args[1]
+    cod = args[1]
     string = list(args[2])
     int_k = int(args[3])
 
-    if shifr == 'e':
-        print(string)
-        int_i = 0
-        for symbol in string:
-            if (int_i + int_k) != len(string):
-                a = symbol
-                string[int_i] = string[int_i + int_k]
-                string[int_i + int_k] = a
-                int_i += 1
-        print(string)
-
-    if shifr == 'd':
-        print(string)
-        int_i = len(string) - 1
-        while int_i >= int_k:
-            a = string[int_i - int_k]
-            string[int_i - int_k] = string[int_i]
-            string[int_i] = a
-            int_i -= 1
-        print(string)
+    Enc_or_dec(cod, string, int_k)
 
 
 if __name__ == '__main__':
