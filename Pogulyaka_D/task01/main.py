b2c96f346aecc1493339450ec1ef1758b2cddbdf
phoals <56293439+phoals@users.noreply.github.com>
2022-02-20 19:57:19 +0300

    Update main.py

--- Pogulyaka_D/task01/main.py
+++ Pogulyaka_D/task01/main.py
@@ -2,9 +2,11 @@ import sys
 import string
 
 
-def caesar_cipher(str_to_work, key):
+digits = '1234567890'
+
+
+def caesar_cipher(str_to_work: str, key: int) -> str:
     abc = string.ascii_lowercase
-    digits = '1234567890'
     str_res = []
     for item in str_to_work:
         if item.isdigit():
@@ -17,19 +19,19 @@ def caesar_cipher(str_to_work, key):
             index = (abc.upper().index(item) + key) % len(abc)
             str_res.append(abc[index].upper())
         else:
-            str_res.append(item)
-    print("".join(str_res))
+            str_res.append(item)  
+    return "".join(str_res)
 
 
 def main(args: list):
     if args[1] == 'e':
-        caesar_cipher(args[2], int(args[3]))
+        print(caesar_cipher(args[2], int(args[3])))
     if args[1] == 'd':
-        caesar_cipher(args[2], -int(args[3]))
+        print(caesar_cipher(args[2], -int(args[3])))
 
 
 if __name__ == '__main__':
     main(sys.argv)
-    #main(['main.py', 'd', "nkvbj1 2145 jgfdkk    TyUo1 && asdjq. uud, uas", '12345'])
-    #main(['main.py', 'e', "BASDioo1234 ka, isd", '10'])
-    #main(['main.py', 'd', "abcd1234 defg567", '1'])
+    # main(['main.py', 'd', "nkvbj1 2145 jgfdkk    TyUo1 && asdjq. uud, uas", '12345'])
+    # main(['main.py', 'e', "BASDioo1234 ka, isd", '10'])
+    # main(['main.py', 'd', "abcd1234 defg567", '1'])
