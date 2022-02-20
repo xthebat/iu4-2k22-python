80012758cf37c3c5a758a80d90e3179f4ab1c682
Mikhail <apakov.m@frtk.ru>
2022-02-20 19:10:31 +0300

    Hotfix + Add tests

--- Apakov_M/task01/main.py
+++ Apakov_M/task01/main.py
@@ -1,6 +1,7 @@
 import sys
 import string
 
+
 def main(args: list):
     if len(args) != 4:
         print('Error: only 4 parameters')
@@ -12,18 +13,24 @@ def main(args: list):
         print(caesar(args[2], int(args[3])))
     elif args[1].lower() == 'd':
         print(caesar(args[2], -int(args[3])))
-    else: 
+    else:
         print('Error: only E or D')
         sys.exit(-3)
+
 
 def caesar(string_to_work: str, shift: int) -> str:
     ALPHABET = string.ascii_lowercase + string.ascii_uppercase + string.digits
     result = []
     for char in string_to_work:
         if char in ALPHABET:
-            result.append(ALPHABET[(ALPHABET.find(char) + shift) % len(ALPHABET)])
+            result.append(
+                ALPHABET[(ALPHABET.find(char) + shift) % len(ALPHABET)])
+        else:
+            result.append(char)
     return "".join(result)
 
-if __name__=="__main__":
-    main(sys.argv)
 
+if __name__ == "__main__":
+    # main(sys.argv)
+    main(["", "e", "make love not war", "10"])
+    main(["", "d", "wkuo vyFo xyD GkB", "10"])
