9654f4016909e21ede5e9765fcd09a2c52858607
Kirill <kirSM2010@gmail.com>
2022-02-20 17:48:42 +0300

    Fixed mistakes from review

--- Kutaev_K/task01/main.py
+++ Kutaev_K/task01/main.py
@@ -1,43 +1,44 @@
 import sys
 
+DICTIONARY = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
-def move_char(char, key):
-    dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
-    for symbol in dictionary:
-        if symbol == char:
-            if dictionary.index(symbol) + int(key) > len(dictionary):
-                key = int(key) - len(dictionary) - 1
-                movedChar = dictionary[key]
-            else:
-                movedChar = dictionary[dictionary.index(symbol) + int(key)]
-    return movedChar
+def move_char(mode: str, char: str, key: int):
+    if mode == "d":
+        key = -key
+    if DICTIONARY.index(char) + key > len(DICTIONARY):
+        key = key - len(DICTIONARY) - 1
+        moved_char = DICTIONARY[key]
+    else:
+        moved_char = DICTIONARY[DICTIONARY.index(char) + key]
+    return moved_char
+
+
+def error(string: str):
+    print(string)
+    sys.exit(-1)
 
 
 def main(args: list):
-    mode = args[1]
-    string = args[2]
-    key = args[3]
-    resList = []
-    i = 0
+    mode: str = args[1]
+    string: str = args[2]
+    key: int = int(args[3])
+
 
     # errors
     if (mode != "e") & (mode != "d"):
-        return print("Invalid 1 arg")
-    if not key.isdigit():
-        return print("3 arg must be a number")
+        error("Invalid 1 arg")
+    if not args[3].isdigit():
+        error("3 arg must be a number")
     if len(args) != 4:
-        return print("Only 3 args")
+        error("Only 3 args")
 
-    charList = list(string)
-    for char in charList:
-        if mode == "e":
-            resList.append(move_char(char, key))
-            i += 1
-        else:
-            resList.append(move_char(char, -int(key)))
-            i += 1
-    res = "".join(resList)
+    # parse args
+    char_list: list = list(string)
+    res_list: list = []
+    for char in char_list:
+        res_list.append(move_char(mode, char, key))
+    res = "".join(res_list)
     print(res)
     return 0
 
