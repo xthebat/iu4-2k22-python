68a8389f9abad671ce4a8749070d5d61fea3c2dc
Valikhan <bushpulbek@gmail.com>
2022-02-21 16:17:52 +0300

    Алексей Алексеевич, извините за второй commit, просто резко осенило, что можно функции decrypt/encrypt объединить в одну. Соответственно, это и сделал

--- Aiguzhin_V/task01/main.py
+++ Aiguzhin_V/task01/main.py
@@ -1,13 +1,12 @@
 import sys
 
-op_type = set("de")
-cyrillic = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя")
-english_capacity = 26
-digit_capacity = 10
 codes = [list(range(48, 58)), list(range(65, 91)), list(range(97, 123))]
 
 
 def arg_check(args: list) -> None:
+    # Local constants
+    op_type = set("de")
+    cyrillic = set("абвгдеёжзийклмнопрстуфхцчшщъыьэюя")
     # Check validity of an operation type
     if args[0] not in op_type:
         sys.exit("Choose either encrypt or decrypt")
@@ -16,64 +15,47 @@ def arg_check(args: list) -> None:
         sys.exit("Remember, no russian")
     # Check validity of a key
     if args[2].isdigit() is False:
-        sys.exit("Key must be integer")
+        sys.exit("Key must be an integer")
 
 
-def decrypt(enc_text: list, key: str) -> str:
-    decr_text = []
-    new_sym = None
-    for x in enc_text:
-        ascii_code = ord(x)
-        # Decrypt only if symbol is in [[0-9], [A-Z], [a-z]]
+def cipher(given_text: list, shift: int) -> str:
+    answer = []
+    digit_capacity = len(codes[0])
+    english_capacity = len(codes[1])
+    for sym in given_text:
+        ascii_code = ord(sym)
+        # Decrypt/encrypt if symbol is in [[0-9], [A-Z], [a-z]]
         if any(ascii_code in sub_codes for sub_codes in codes):
             # 0-9 cycle
             if ascii_code in codes[0]:
-                new_sym = (ascii_code - int(key) - codes[0][0]) % digit_capacity + codes[0][0]
+                new_sym = (ascii_code + shift -
+                           codes[0][0]) % digit_capacity + codes[0][0]
             # A-Z cycle
             elif ascii_code in codes[1]:
-                new_sym = (ascii_code - int(key) - codes[1][0]) % english_capacity + codes[1][0]
+                new_sym = (ascii_code + shift -
+                           codes[1][0]) % english_capacity + codes[1][0]
             # a-z cycle
             elif ascii_code in codes[2]:
-                new_sym = (ascii_code - int(key) - codes[2][0]) % english_capacity + codes[2][0]
+                new_sym = (ascii_code + shift -
+                           codes[2][0]) % english_capacity + codes[2][0]
+        # Leave spaces, special characters, etc. as they are
         else:
             new_sym = ascii_code
-        # Leave spaces, special characters etc. as they are
-        decr_text.append(chr(new_sym))
-    return "".join(decr_text)
-
-
-def encrypt(dec_text: list, key: str) -> str:
-    enc_text = []
-    new_sym = None
-    for x in dec_text:
-        ascii_code = ord(x)
-        # Encrypt only if symbol is in [[0-9], [A-Z], [a-z]]
-        if any(ascii_code in sub_codes for sub_codes in codes):
-            # 0-9 cycle
-            if ascii_code in codes[0]:
-                new_sym = (ascii_code + int(key) - codes[0][0]) % digit_capacity + codes[0][0]
-            # A-Z cycle
-            elif ascii_code in codes[1]:
-                new_sym = (ascii_code + int(key) - codes[1][0]) % english_capacity + codes[1][0]
-            # a-z cycle
-            elif ascii_code in codes[2]:
-                new_sym = (ascii_code + int(key) - codes[2][0]) % english_capacity + codes[2][0]
-        # Leave spaces, special characters etc. as they are
-        else:
-            new_sym = ascii_code
-        enc_text.append(chr(new_sym))
-    return "".join(enc_text)
+        answer.append(chr(new_sym))
+    return "".join(answer)
 
 
 def main(args: list) -> None:
     # Check for validity of the arguments
     arg_check(args)
+
+    key = int(args[2])
     # Decrypt
     if args[0] == "d":
-        print(decrypt(args[1], args[2]))
+        print(cipher(args[1], -key))
     # Encrypt
     elif args[0] == "e":
-        print(encrypt(args[1], args[2]))
+        print(cipher(args[1], key))
 
 
 if __name__ == '__main__':
