32d8791769a9236f3a49ef7a0994dddabc3e0811
gadzhiev_v <vazhid2000@mail.ru>
2022-03-01 23:42:59 +0300

    comm

---
+++ gadzhiev/task02/main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+
+def collect(current_dir: str):
+    os.chdir(current_dir)
+    list_of_dirs = [current_dir]
+    only_dirs = []
+    everything = os.listdir(".")
+    if len(everything) != 0:
+        for elem in everything:
+            if os.path.isdir(everything[everything.index(elem)]):
+                only_dirs.append(everything[everything.index(elem)])
+        for elem in only_dirs:
+            list_of_dirs.append(collect(elem))
+    os.chdir("..")
+    return list_of_dirs
+
+
+def print_list_of_dirs(directory: list, lev: int = 0):  # Аккуратно своровано и немного исправлено
+    for index in directory:
+        if isinstance(index, list):
+            print_list_of_dirs(index, lev + 1)
+        else:
+            print("|" + " |" * lev + " |> " + index)
+
+
+def main(args: list):
+    while not os.path.isdir(args[1]):
+        os.chdir("..")
+    print_list_of_dirs(collect(args[1]))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
