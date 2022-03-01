7b20ee79f24c95b310a33875fb8f6993a733b5a2
Vyacheslav Rozhkovskiy <viking20131@gmailc.om>
2022-03-02 00:42:56 +0300

    after review task2

--- rozhkovsky_v/task02/main.py
+++ rozhkovsky_v/task02/main.py
@@ -9,37 +9,42 @@ class Directory:
         self.files = list()
         self.dirs = list()
 
+    # тут должен был быть еще один перегруженный конструктор но он пал жертвой питона
 
-# Print all by depth search
-# step - вложенность(не придумал как это лучше по английски назвать, вся надежда на ревью)
-def print_all_rec(root_dir: Directory, step=0):
-    spacer = " -" * step
-    print(spacer, root_dir.name)
-    for file_name in root_dir.files:
-        print(spacer, " - ", file_name, sep='')
-    for directory in root_dir.dirs:
-        print_all_rec(directory, step + 1)
+    def add_file(self, file: str):
+        self.files.append(file)
 
+    def add_dir(self, dir):
+        self.dirs.append(dir)
 
-# Set lists files and dirs to object
-# work like depth search for dirs
-def set_dirs_rec(root_dir: Directory):
-    for name in os.listdir(root_dir.name):
-        new_name = root_dir.name + "/" + name
-        if os.path.isdir(new_name):
-            new_dir = Directory(new_name)
-            root_dir.dirs.append(new_dir)
-            set_dirs_rec(new_dir)
-        else:
-            root_dir.files.append(name)
+    # Print all by depth search
+    def print(self, depth=0):
+        spacer = " -" * depth
+        print(spacer, self.name)
+        for file_name in self.files:
+            print(spacer, " - ", file_name, sep='')
+        for directory in self.dirs:
+            directory.print(depth + 1)
+
+    # Set lists files and dirs to object
+    # work like depth search for dirs
+    def parse(self):
+        for name in os.listdir(self.name):
+            new_name = os.path.join(self.name, name)
+            if os.path.isdir(new_name):
+                new_dir = Directory(new_name)
+                self.dirs.append(new_dir)
+                new_dir.parse()
+            else:
+                self.files.append(name)
 
 
 def main(args: list):
     root_dir_name = args[1]
     root_dir = Directory(root_dir_name)
 
-    set_dirs_rec(root_dir)
-    print_all_rec(root_dir)
+    root_dir.parse()
+    root_dir.print()
 
 
 if __name__ == '__main__':
