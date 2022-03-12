34b706d9a8badca8b84c3cd9fa7665902050c1c0
Kirill Mironov <www.kirillmironov.2000@gmail.com>
2022-03-13 00:23:20 +0300

    task04.1
    Выполнил сначала третье дз через comprehension, но пришлось перелопатить под исключения

--- Mironov_K/task03/main.py
+++ Mironov_K/task03/main.py
@@ -2,64 +2,90 @@ import sys
 import os
 
 
-def pars_user_line(str_line: str) -> (str, str):
-    index = [i for i, char in enumerate(str_line) if char == ";"]
-    yt_name = str_line[index[2] + 1:index[3]]
-    real_name = str_line[index[0] + 1:index[1]]
-    return yt_name, real_name
+class FileExtError(Exception):
+
+    def __init__(self, file: str, ext: str):
+        self.file = file
+        self.ext = ext
+
+
+class ChatParseError(Exception):
+
+    def __init__(self, line: str):
+        self.line = line
+
+
+# По сути функция добавления имен пользователей дополнительная, поэтому при наличии ошибок вывожу пустой словарь
 
 
 def gen_user_dict(path_file: str) -> dict:
-    list_lines = list(filter(lambda it: it[0].isdigit(), open(path_file, "rt").readlines()))
-    dict_result = dict(map(pars_user_line, list_lines))
-    if "" in dict_result:
-        del dict_result[""]
-    return dict_result
+    try:
+        if os.path.splitext(path_file)[1] != ".csv":
+            raise FileExtError(path_file, "csv")
+        list_lines = open(path_file, "rt").readlines()
+    except FileNotFoundError as error:
+        print(error)
+        return {}
+    except FileExtError as error:
+        print(f"File extention error: {error.file} is not {error.ext}")
+        return {}
+    else:
+        list_users = (it.split(";") for it in list_lines)
+        dict_result = {it[3]: it[1] for it in list_users if it[0].isdigit() and it[3] != ""}
+        return dict_result
 
 
 def gen_chat_dict(path_file: str) -> dict:
-    list_message = [it.split("\n?") for it in open(path_file, "rt").read().split("\n\n")]
-    dict_result = dict()
-    for it in list_message:
-        dict_result.setdefault(it[0], list())
-        dict_result[it[0]].append(it[1])
-    return dict_result
+    try:
+        if os.path.splitext(path_file)[1] != ".txt":
+            raise FileExtError(path_file, "txt")
+        list_chat = open(path_file, mode="rt", encoding="UTF-16").read().split("\n\n")
+        for it in list_chat:
+            if it.count("\n\u200b") != 1:
+                raise ChatParseError(it)
+        # Не придумал ничего лучше для исключения чем прогонка через фор
+        # list_message = [it.split("\n\u200b") for it in list_chat if it.count("\n\u200b") == 1]
+        list_message = [it.split("\n\u200b") for it in list_chat]
+        # При копировании чата с ютуба перед комметарием есть дополнительный символ юникода: пробел нулевой ширины
+    except FileNotFoundError as error:
+        sys.exit(error)
+    except FileExtError as error:
+        sys.exit(f"File extention error: {error.file} is not {error.ext}")
+    except UnicodeDecodeError as error:
+        sys.exit(f"File {path_file} codec error: {error}")
+    except ChatParseError as error:
+        sys.exit(f"Error format file {path_file} in message:\n {error.line}")
+    else:
+        dict_result = dict()
+        for it in list_message:
+            yt_name = it[0]
+            dict_result.setdefault(yt_name, list())
+            dict_result[yt_name].append(it[1])
+        return dict_result
 
 
 def print_chat(user_dict: dict, chat_dict: dict):
-    for item in chat_dict:
-        print(item + " aka " + user_dict.get(item, "not declared"))
-        print("    Кол-во сообщений: " + str(len(chat_dict[item])))
-        print("    Кол-во символов: " + str(sum(map(len, chat_dict[item]))))
-        for mess in chat_dict[item]:
-            print(" -" + mess)
-        print()
+    for yt_name in chat_dict:
+        real_name = user_dict.get(yt_name, "not declared")
+        print(f"\n{yt_name} aka {real_name}")
+        print(f"\tКол-во сообщений: {len(chat_dict[yt_name])}")
+        print(f"\tКол-во символов: {sum(map(len, chat_dict[yt_name]))}")
+        for mess in chat_dict[yt_name]:
+            print(f" -{mess}")
 
 
 def main(args: list):
-
-    if not os.path.exists(args[1]):
-        sys.exit("User file is not exist")
-
-    if not os.path.exists(args[2]):
-        sys.exit("Chat file is not exist")
-
-    if args[1][-4:] != ".csv":
-        sys.exit("User file is not csv")
-
-    if args[2][-4:] != ".txt":
-        sys.exit("Chat file is not txt")
-
     user_dict = gen_user_dict(args[1])
     chat_dict = gen_chat_dict(args[2])
     print_chat(user_dict, chat_dict)
 
 
-# chat file is txt in ANSI format
-
-
 if __name__ == '__main__':
     # main(sys.argv)
-    main(["main.py", "./ИУ4 - МЛНИ - 2022.csv", "./chat.txt"])
+     main(["main.py", "./ИУ4 - МЛНИ - 2022.csv", "./chat_ideal.txt"])
+    # main(["main.py", "./ИУ4 - МЛНИ - 2022.csv", "./chat_codecUTF8.txt"])
+    # main(["main.py", "./ИУ4 - МЛНИ - 2022.csv", "./chat_codecANSI.txt"])
+    # main(["main.py", "./ИУ4 - МЛНИ - 2022.csv", "./chat_messageerror.txt"])
+    # main(["main.py", "./ИУ4 - МЛНИ - 2022.csv", "./chat_messageerror2.txt"])
     # main(["main.py", "./chat.txt", "./chat.txt"])
     # main(["main.py", "./ИУ4 - МЛНИ - 2022.csv", "./ИУ4 - МЛНИ - 2022.csv"])
