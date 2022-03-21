044006e4690966c0bbf3ddfccd0e17e4d5597974
phoals <56293439+phoals@users.noreply.github.com>
2022-03-21 19:49:54 +0300

    Create main.py

---
+++ Pogulyaka_D_I/task05/main.py
@@ -0,0 +1,18 @@
+import json
+import os
+from typing import Iterable, List
+
+from demo import Round, _fix_rounds, Match, get_players_nicknames, Player
+
+
+def main():
+    with open("1-266bdc4c-3672-4cd5-bc97-b79c6c1e4d6a-1-1.json", "rt") as file:
+        text = file.read()
+        data = json.loads(text)
+
+    match = Match.from_data(data)
+    match.players_print()
+
+
+if __name__ == '__main__':
+    main()
