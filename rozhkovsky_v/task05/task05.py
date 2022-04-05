1fb5fe804011b8766bfda1d7817557b13155c649
Vyacheslav <31929848+pRockrammer@users.noreply.github.com>
2022-04-05 11:58:56 +0300

    Rozhkovsky_V-Task05 (#70)
    * Add task01
    
    * Приведение к условию
    
    * Подправил аргументы
    
    * Починка кривого комита и добавление тестов
    
    * After code review
    
    Предлагаю остановиться на таком варианте
    
    * init commit
    
    * remove task1
    
    * after review task2
    
    * add task03
    
    * Приведение к условию
    
    * after review task03
    
    Ну, как-то так
    
    * Upgrade to Task04
    
    * task05
    
    * забыл 3 таск убрать
    
    * task05 v2.0 alpha
    
    Need to debug
    
    * score fix
    
    * little player fix
    
    * task05 v2.0 release
    
    * print_info upgrade
    
    теперь это читабельно
    
    Co-authored-by: Vyacheslav Rozhkovskiy <viking20131@gmailc.om>

---
+++ rozhkovsky_v/task05/task05.py
@@ -0,0 +1,301 @@
+import sys
+import json
+import pandas
+from typing import List
+from dataclasses import dataclass
+
+
+class Match:
+    match_id: str
+    map_name: str
+    team_a: str
+    team_b: str
+    rounds: List['Round']
+    players: List['Player']
+
+    def __init__(self):
+        self.rounds = list()
+        self.players = list()
+
+    # не придумал как это лучше назвать
+    # если игрок новый то добавляем его, иначе возвращаем из списка
+    def add_get_player(self, player: 'Player') -> 'Player':
+        if player not in self.players:
+            self.players.append(player)
+            return player
+        else:
+            return self.players[self.players.index(player)]
+
+    def parse(self, file_name):
+        with open(file_name, "rt") as file:
+            text = file.read()
+            data = json.loads(text)
+        self.match_id = data["matchID"]
+        self.map_name = data["mapName"]
+
+        for data_round in data["gameRounds"]:
+            cs_round = Round()
+            cs_round.parse(data_round)
+
+            if cs_round.is_normal():
+                self.rounds.append(cs_round)
+
+    def set_players(self):
+        self.__parse_players_by_kills()
+        self.__parse_players_by_damages()
+        self.__parse_players_by_fires()
+        self.__set_players_survive()
+        self.__set_players_kast()
+
+    def print_info(self):
+        half_round = self.rounds[len(self.rounds) // 2]
+        last_round = self.rounds[-1]
+        header = "Player K D A HS% ACC% ADR UD KAST% Rating"
+        data_frame = pandas.DataFrame(columns=header.split(" "))
+        print(f"Match {self.match_id} {self.map_name}")
+        print(f"{self.rounds[0].win_team} vs {self.rounds[0].lose_team}")
+        print(f"First Half:  {half_round.score._end_ct_score} : {half_round.score._end_t_score}")
+        print(f"Second Half: {last_round.score._end_t_score - half_round.score._end_ct_score} :",
+              f"{last_round.score._end_ct_score - half_round.score._end_t_score}")
+        print(f"Final score: {last_round.score._end_t_score} : {last_round.score._end_ct_score}")
+        for player in self.players:
+            headshots_rate = format(100 * player.headshots / player.kills if player.kills != 0 else 0, '.2f')
+            accuracy_rate = format(100 * (player.shots_done - player.shots_missed) /
+                                   player.shots_done if player.shots_done != 0 else 0, '.2f')
+            damage_rate = player.total_damage / len(self.rounds)
+            kast_rate = player.kast_rounds / len(self.rounds)
+            # rating 2.0
+            kpr = player.kills / len(self.rounds)
+            dpr = player.deaths / len(self.rounds)
+            impact = 2.13 * kpr + 0.42 * player.assists / len(self.rounds) - 0.41
+            rating = format(0.0073 * kast_rate + 0.3591 * kpr - 0.5329 * dpr +
+                            0.2372 * impact + 0.0032 * damage_rate + 0.1587, '.3f')
+            data_dict = dict()
+            data_dict["Player"] = player.name
+            data_dict["K"] = player.kills
+            data_dict["D"] = player.deaths
+            data_dict["A"] = player.assists
+            data_dict["HS%"] = headshots_rate
+            data_dict["ACC%"] = accuracy_rate
+            data_dict["ADR"] = format(damage_rate, '.2f')
+            data_dict["UD"] = player.utility_damage
+            data_dict["KAST%"] = format(kast_rate * 100, '.2f')
+            data_dict["Rating"] = rating
+            data_frame = data_frame.append(data_dict, ignore_index=True)
+        print(data_frame)
+
+    def __parse_players_by_kills(self):
+        for cs_round in self.rounds:
+            for kill in cs_round.kills:
+                attacker = self.add_get_player(Player(kill.attacker_name, kill.attacker_team))
+                victim = self.add_get_player(Player(kill.victim_name, kill.victim_team))
+                if kill.assister_name is not None:
+                    assister = self.add_get_player(Player(kill.assister_name, kill.assister_team))
+                    assister.assists += 1
+
+                attacker.shots_done += 1
+                attacker.kills += 1
+                victim.deaths += 1
+
+                if kill.is_trade and kill.trader_name == attacker.name:
+                    attacker.trades += 1
+                elif kill.is_trade and kill.trader_name == victim.name:
+                    victim.trades += 1
+
+                if kill.is_headshot:
+                    attacker.headshots += 1
+
+    def __parse_players_by_damages(self):
+        for cs_round in self.rounds:
+            for damage in cs_round.damages:
+                attacker = self.add_get_player(Player(damage.attacker_name, damage.attacker_team))
+                vicitim = self.add_get_player(Player(damage.victim_name, damage.victim_team))
+
+                attacker.shots_done += 1
+                attacker.total_damage += damage.hp_damage_taken
+                if damage.weapon == "HE Grenade" or damage.weapon == "Molotov":
+                    attacker.utility_damage += damage.hp_damage_taken
+
+    def __parse_players_by_fires(self):
+        for cs_round in self.rounds:
+            for fire in cs_round.fires:
+                shooter = Player(fire.player_name, fire.player_team)
+                shooter = self.add_get_player(shooter)
+                shooter.shots_missed += 1
+                shooter.shots_done += 1
+
+    def __set_players_survive(self):
+        count_of_rounds = len(self.rounds)
+        for player in self.players:
+            player.survives = count_of_rounds - player.deaths
+
+    def __set_players_kast(self):
+        for player in self.players:
+            for cs_round in self.rounds:
+                if cs_round.is_player_kast(player):
+                    player.kast_rounds += 1
+
+
+class Round:
+    score: 'Score'
+    kills: List['Kill']
+    damages: List['Damage']
+    fires: List['Fire']
+    win_team: str
+    lose_team: str
+
+    def __init__(self):
+        self.kills = list()
+        self.damages = list()
+        self.fires = list()
+
+    def parse(self, data_round):
+        self.score = Score(data_round)
+        self.win_team = data_round["winningTeam"]
+        self.lose_team = data_round["losingTeam"]
+        for data_kill in data_round["kills"]:
+            kill = Kill(data_kill)
+            self.kills.append(kill)
+
+        for data_damage in data_round["damages"]:
+            damage = Damage(data_damage)
+            self.damages.append(damage)
+
+        for data_fire in data_round["weaponFires"]:
+            fire = Fire(data_fire)
+            self.fires.append(fire)
+
+    def is_player_kast(self, player: 'Player') -> bool:
+        is_kill = False
+        is_assist = False
+        is_survive = True
+        is_trade = False
+        for kill in self.kills:
+            if kill.is_assister(player):
+                is_assist = True
+            if kill.is_trader(player):
+                is_trade = True
+            if kill.is_victim(player):
+                is_survive = False
+            if kill.is_killer(player):
+                is_kill = True
+
+        return is_kill or is_assist or is_survive or is_trade
+
+    def is_normal(self) -> bool:
+        return self.score._end_t_score > self.score._t_score or \
+               self.score._end_ct_score > self.score._ct_score
+
+
+class Kill:
+    attacker_name: str
+    attacker_team: str
+    victim_name: str
+    victim_team: str
+    assister_name: str
+    assister_team: str
+    trader_name: str
+    is_headshot: bool
+    weapon: str
+    is_trade: bool
+
+    def __init__(self, data_kill):
+        self.attacker_name = data_kill["attackerName"]
+        self.attacker_team = data_kill["attackerTeam"]
+        self.victim_name = data_kill["victimName"]
+        self.victim_team = data_kill["victimTeam"]
+        self.assister_name = data_kill["assisterName"]
+        self.assister_team = data_kill["assisterTeam"]
+        self.trader_name = data_kill["playerTradedName"]
+        self.is_headshot = data_kill["isHeadshot"]
+        self.weapon = data_kill["weapon"]
+        self.is_trade = data_kill["isTrade"]
+
+    def is_assister(self, player: 'Player') -> bool:
+        return self.assister_name == player.name
+
+    def is_trader(self, player: 'Player') -> bool:
+        return self.is_trade and self.trader_name == player.name
+
+    def is_victim(self, player: 'Player') -> bool:
+        return self.victim_name == player.name
+
+    def is_killer(self, player: 'Player') -> bool:
+        return self.assister_name == player.name
+
+
+class Damage:
+    attacker_name: str
+    attacker_team: str
+    victim_name: str
+    victim_team: str
+    hp_damage_taken: int
+    weapon: str
+
+    def __init__(self, data_damage):
+        self.attacker_name = data_damage["attackerName"]
+        self.attacker_team = data_damage["attackerTeam"]
+        self.victim_name = data_damage["victimName"]
+        self.victim_team = data_damage["victimTeam"]
+        self.hp_damage_taken = data_damage["hpDamageTaken"]
+        self.weapon = data_damage["weapon"]
+
+
+class Fire:
+    player_name: str
+    player_team: str
+    weapon: str
+
+    def __init__(self, data_fire):
+        self.player_name = data_fire["playerName"]
+        self.player_team = data_fire["playerTeam"]
+        self.weapon = data_fire["weapon"]
+
+
+class Score:
+    _t_score: int
+    _ct_score: int
+    _end_t_score: int
+    _end_ct_score: int
+
+    def __init__(self, data_round):
+        self._t_score = data_round["tScore"]
+        self._ct_score = data_round["ctScore"]
+        self._end_t_score = data_round["endTScore"]
+        self._end_ct_score = data_round["endCTScore"]
+
+
+@dataclass
+class Player:
+    name: str
+    team: str
+    kills: int = 0
+    deaths: int = 0
+    assists: int = 0
+    survives: int = 0
+    trades: int = 0
+    shots_done: int = 0
+    shots_missed: int = 0
+    headshots: int = 0
+    total_damage: int = 0
+    utility_damage: int = 0
+    kast_rounds: int = 0
+
+    def __eq__(self, other):
+        return self.name == other.name and self.team == other.team
+
+
+def main(args):
+    if len(args) < 2:
+        sys.exit(-1)
+
+    file_name = args[1]
+
+    match = Match()
+    match.parse(file_name)
+    match.set_players()
+    match.print_info()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
